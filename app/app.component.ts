f43de006d9bd8ea117d5340312c53fa9672cb886
Agnieszka Matysek <amatysek@fractalsoft.org>
2016-09-12 21:11:36 +0200

    Step3 - Second component with Hero model

--- app/app.component.ts
+++ app/app.component.ts
@@ -52,20 +52,16 @@ import { Component } from '@angular/core';
       }
     `],
     template: `
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+  <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+    <li *ngFor="let hero of heroes"
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 })
 export class AppComponent {
