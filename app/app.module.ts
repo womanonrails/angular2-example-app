daea3e48db0e311d33c21e77eab8c7e2b97092c8
Agnieszka Matysek <amatysek@fractalsoft.org>
2016-09-12 21:31:00 +0200

    Step5 - Selected heroes dispay on dashboard, first html template

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }        from './app.component';
 import { routing }             from './app.routing';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
